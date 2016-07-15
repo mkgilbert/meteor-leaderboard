6143f780becf28ee8ff0903bfd99a7b6e29bfced
Michael Gilbert <mike@nau.edu>
2016-07-15 12:51:06 -0700

    comment in setup about how to run meteor

--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ rm -rf leaderboard/client leaderboard/server
 cp -r /tmp/leaderboard/* leaderboard/
 
 meteor add session
+
+# to run, go to leaderboard/ directory and do `meteor run`
 echo Done
 exit 0
